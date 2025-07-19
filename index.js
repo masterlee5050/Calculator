c2c0ab402134442249779f2c87945578764d1116
Master_Lee <141339677+masterlee5050@users.noreply.github.com>
2025-07-18 18:51:58 -0700

    added LL

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 let x = 10 ;
 let y = x++
+class  Car{
+car = "hello";
+car2 = 67;
+constructor(h,b,car ="u"){
+this.h = h;
+this.b = b;
+this.car = car;
+}
+static hh(){
+console.log(this.h,this.car,this.b)
+}
+}
+let carL = new Car("hell","world");
+Car.hh(carL);
+let array2 = [71,0,3,4,5,7];
+let [a,b,...c] = array2;
+let array2sort = array2.toSorted()
+console.log(array2sort)
+let gg = Array.from(array2);
+for (bb of array2){
+    console.log(bb)
+}
